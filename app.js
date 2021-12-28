ee1525575dd2c4fe6ad47d9887ee2f445bcd1bc3
Rutuja Jagdale <82458146+rutuja332@users.noreply.github.com>
2021-12-28 19:46:47 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,17 @@
+// let video = window.document.querySelector('#video');
+// let video1 = window.document.querySelector('#video1');
+// if(navigator.mediaDevices && navigator.mediaDevices.getUserMedia){
+//     navigator.mediaDevices.getUserMedia({video : true}).then(stream =>{
+//         video.srcObject = stream;
+//         video.play();
+//         video1.srcObject = stream;
+        
+//         video.play();
+//     })
+// }
+// if(navigator.mediaDevices && navigator.mediaDevices.getUserMedia){
+//     navigator.mediaDevices.getUserMedia({video : true}).then(stream =>{
+//         video.play();
+
+//     })
+// }
